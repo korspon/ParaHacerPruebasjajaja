28b91a2839873aaf0ff35fef209d778616fe05bc
korspon <korsponprotonmail.com>
2021-10-04 21:47:16 -0500

    Codigo modificado

--- hola.cpp
+++ hola.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main(){
+  int a = 0;
 
   cout << "Hola mundo a jajja desde arch";
+
+  if( a == 0 ){
+    cout << "Estamos en la rama dev jaja";
+  }
 
   return 0;
 }
